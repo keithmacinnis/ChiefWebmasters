f80ee4dc23a734fe215cade67299f35f1e3a6897
Keith MacInnis <keith@keithmacinnis.com>
2021-07-15 11:14:56 -0700

    init

---
+++ formPoster.php
@@ -0,0 +1,31 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$bucket =  $_ENV['BUCKET'];
+$bucket_pw = $_ENV['BUCKET_PW'];
+
+$link = mysqli_connect("localhost", $bucket, $bucket_pw, "chiefweb_emailTickets");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Escape user inputs for security
+$first_name = mysqli_real_escape_string($link, $_REQUEST['name']);
+$email = mysqli_real_escape_string($link, $_REQUEST['email']);
+$msg = mysqli_real_escape_string($link, $_REQUEST['message']);
+ 
+// Attempt insert query execution
+$sql = "INSERT INTO emails (first_name, email, msg) VALUES ('$first_name', '$email', '$msg')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
---
+++ formQuoteEmailer.php
@@ -0,0 +1,52 @@
+<?php
+
+$error_output = '';
+$success_output = '';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+// Build POST request to get the reCAPTCHA v3 score from Google
+$recaptcha_url = $_ENV['RECAPTCHA_URL'];
+$recaptcha_secret =  $_ENV['$RECAPTCHA_SECRET'];
+$recaptcha_response = $_POST['recaptcha_response'];
+$to_email = $_ENV['TO_EMAIL'];
+
+// Make and decode POST request
+$recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+$recaptcha = json_decode($recaptcha);
+
+// Take action based on the score returned
+if ($recaptcha->success == true && $recaptcha->score >= 0.5 && $recaptcha->action == 'contact') {
+    // This is a human, send email
+      $name;$visitor_email;$message;$category;$status;$copy;
+      $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+      $visitor_email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+      $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+      $category = filter_input(INPUT_POST, 'category', FILTER_SANITIZE_STRING);
+      $status = filter_input(INPUT_POST, 'status', FILTER_VALIDATE_EMAIL);
+      $copy = filter_input(INPUT_POST, 'copy', FILTER_SANITIZE_STRING);
+      $email_from = $visitor_email;
+      $email_subject = "New Form submission";
+      $email_body = "You have received a new message from the user $name via wbs with a score of $recaptcha->score.\n".
+        "You have received a new $category quote request from the user $name via wbs.\n".
+        "The project has a status of: $status \n".
+        "Here is the copy status:\n $copy \n".    
+        "Here is the message:\n $message \n".         
+      $to = $to_email;
+      $headers = "From: $email_from \r\n";
+      $headers .= "Reply-To: $visitor_email \r\n";
+      mail($to,$email_subject,$email_body,$headers);
+    $success_output = "Your message sent successfully";
+} else {
+    // Score less than 0.5 indicates suspicious activity. Return an error
+    $error_output = "Something went wrong. Please try again later";
+}
+
+$output = array(
+    'error'     =>  $error_output,
+    'success'   =>  $success_output
+);
+
+// Output needs to be in JSON format
+echo json_encode($output);
+
+?>
